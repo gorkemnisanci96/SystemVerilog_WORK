05dcc82f0a3b107465ae0556542d3a3944b38c78
gorkemnisanci96 <70295884+gorkemnisanci96@users.noreply.github.com>
2020-12-07 08:04:20 -0700

    Add files via upload

--- stepBYstep/CLASSES.sv
+++ stepBYstep/CLASSES.sv
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/07/2020 07:49:28 AM
-// Design Name: 
-// Module Name: CLASSES
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+// System Verilog CLASSSES examples 
+
 
 
 module classes_ex1 ();
